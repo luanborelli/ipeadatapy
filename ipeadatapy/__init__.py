b9513c76bfda365c93412cc9218a2e598f262a34
Luan Borelli <35273857+luanborelli@users.noreply.github.com>
2019-05-07 16:12:56 -0300

    Fixing importing problem

--- ipeadatapy/__init__.py
+++ ipeadatapy/__init__.py
@@ -4,7 +4,7 @@ import pandas as pd
 from .api_call import api_call
 from .sources import sources
 from .metadata_old import metadata_old
-from .metadata_odata4 import metadata
+from .metadata import metadata
 from .list_series import list_series
 from .describe import describe
 from .timeseries import timeseries
