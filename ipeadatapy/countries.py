842e3efe503136d6abb9ea6c3de9a9ee583e5128
Luan Borelli <35273857+luanborelli@users.noreply.github.com>
2019-05-08 15:29:36 -0300

    Fixed some problems

--- ipeadatapy/countries.py
+++ ipeadatapy/countries.py
@@ -4,7 +4,7 @@ from .api_call import api_call
 def countries(country_id=None, keyword=None):
     api = "http://ipeadata2-homologa.ipea.gov.br/api/v1/Paises"
     cReturn = api_call(api)[['PAICODIGO','PAINOME']].rename(index=str, columns={"PAICODIGO": "ID", "PAINOME": "COUNTRY"})
-    if id is not None:
+    if country_id is not None:
         cReturn = cReturn.loc[cReturn["ID"] == country_id]
     if keyword is not None:
         cReturn = cReturn[cReturn['COUNTRY'].str.contains(keyword)]
