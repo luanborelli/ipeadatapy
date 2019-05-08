026578dd86c95b5d85dd45ec49ef2eba939cd71f
Luan Borelli <35273857+luanborelli@users.noreply.github.com>
2019-05-08 15:32:02 -0300

    Renamed.
    metadata_odata4.py is now metadata.py
    Old metadata.py is now metadata_old.py.

--- ipeadatapy/metadata_odata4.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from .api_call import api_call
-
-def metadata_odata4(series=None):
-    """If no keyword is specified, returns a data frame containing all Ipeadata's time series. Else, returns only the ones that contains the specified keyword in their names."""
-    pos_fix = "('%s')" % series if series is not None else ""
-    api = "http://www.ipeadata.gov.br/api/odata4/Metadados%s" % pos_fix
-    return api_call(api).rename(index=str, columns={"TEMNOME": "THEME", "BASNOME": "BIG THEME", "FNTNOME": "SOURCE", "FNTSIGLA": "SOURCE ACRONYM", "FNTURL": "SOURCE URL", "MULNOME": "UNIT", "PAICODIGO": "COUNTRY", "PERNOME": "FREQUENCY", "SERATUALIZACAO": "LAST UPDATE", "SERCODIGO": "CODE", "SERCOMENTARIO": "COMMENT", "SERNOME": "NAME", "SERNUMERICA": "NUMERICA", "SERSTATUS": "SERIES STATUS", "TEMCODIGO": "THEME CODE", "UNINOME": "MEASURE"})
